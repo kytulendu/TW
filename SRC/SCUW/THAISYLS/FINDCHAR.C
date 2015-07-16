49c52122a2199d412a341645d919cee250b4a912
Khral Steelforge <garuda2550@gmail.com>
2015-07-16 21:31:07 +0700

    SCUW : Remove warnings.
    Signed-off-by: Khral Steelforge <garuda2550@gmail.com>

--- SRC/SCUW/THAISYLS/FINDCHAR.C
+++ SRC/SCUW/THAISYLS/FINDCHAR.C
@@ -16,10 +16,10 @@
 #include <stdlib.h>
 
 short int findchar( unsigned char ch, unsigned char *strbuf ) {
-	while ( *strbuf != NULL && ch >= *strbuf ) {
+	while ( strbuf != NULL && ch >= *strbuf ) {
 		if ( ch == *( strbuf++ ) ) {
 			return( 1 );
 		}
 	}
-	return( NULL );
+	return( 0 );
 }
